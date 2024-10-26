4e5d69c707b2529f40fc727f6e5af0cc905dd1bb
farhadmammadli <farhadmammadli96@gmail.com>
2024-10-26 19:07:59 +0400

    refactor program class

--- CodingTask.Host/Program.cs
+++ CodingTask.Host/Program.cs
@@ -1,50 +1,132 @@
+using CodingTask.Application.Interfaces;
+using CodingTask.Application.Services;
 using CodingTask.Data;
-using Microsoft.AspNetCore.Hosting;
+using CodingTask.Host.Middlewares;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using System;
+using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
-namespace CodingTask.Host
+namespace CodingTask.Host;
+
+public static class Program
 {
-    public class Program
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
+        var builder = WebApplication.CreateBuilder(args);
+
+        var envName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        builder.Configuration.AddJsonFile($"appsettings.{envName}.json", optional: true, reloadOnChange: true);
+
+        builder.Services.AddCors(options =>
         {
-            var host = CreateHostBuilder(args).Build();
-
-            CreateDbIfNotExists(host);
-
-            host.Run();
-        }
-
-        private static void CreateDbIfNotExists(IHost host)
-        {
-            using (var scope = host.Services.CreateScope())
+            options.AddDefaultPolicy(builder =>
             {
-                var services = scope.ServiceProvider;
+                builder
+                       .SetIsOriginAllowed(hostname => true)
+                       .AllowAnyMethod()
+                       .AllowAnyHeader()
+                       .AllowCredentials();
+            });
+        });
 
-                try
-                {
-                    var context = services.GetRequiredService<CodingTaskContext>();
-                    var task = Task.Run(async () => await context.Initialize());
 
-                    task.Wait();
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occurred creating the DB.");
-                }
+        builder.Services.AddControllers().AddJsonOptions(options =>
+        {
+            options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+        });
+
+        builder.Services.AddEndpointsApiExplorer();
+
+        builder.Services.AddSwaggerGen(option =>
+        {
+            option.SwaggerDoc("v1", new OpenApiInfo { Title = "Demo API", Version = "v1" });
+            option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+            {
+                In = ParameterLocation.Header,
+                Description = "Please enter a valid token",
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                BearerFormat = "JWT",
+                Scheme = "Bearer"
+            });
+            option.AddSecurityRequirement(new OpenApiSecurityRequirement
+            {
+    {
+        new OpenApiSecurityScheme
+        {
+            Reference = new OpenApiReference
+            {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
             }
+        },
+        new string[] {}
+    }
+            });
+        });
+
+        builder.Services.AddAuthentication(options =>
+        {
+            options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+            options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        })
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["JWT:Key"]!)),
+                ClockSkew = TimeSpan.Zero
+            };
+        });
+
+
+        builder.Services.AddDbContext<CodingTaskContext>(options =>
+          options.UseInMemoryDatabase("Test"));
+
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<IAuthService, AuthService>();
+        builder.Services.AddScoped<IProductService, ProductService>();
+        builder.Services.AddScoped<IOrderService, OrderService>();
+        builder.Services.AddScoped<ICartService, CartService>();
+
+
+        var app = builder.Build();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var context = scope.ServiceProvider.GetRequiredService<CodingTaskContext>();
+            var task = Task.Run(context.Initialize);
+            task.Wait();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Microsoft.Extensions.Hosting.Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+
+        app.UseRouting();
+        app.UseStaticFiles();
+        app.UseSwagger();
+        app.UseSwaggerUI();
+
+        app.UseAuthentication();
+        app.UseCors();
+        app.UseAuthorization();
+        app.UseMiddleware<ErrorHandlerMiddleware>();
+
+        // Add UserIdMiddleware to the pipeline for demo purposes
+        app.UseMiddleware<UserIdMiddleware>();
+
+        app.MapControllers();
+        app.Run();
     }
 }
